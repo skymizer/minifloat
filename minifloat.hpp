0fb1779f5c966e21cb26d6c492405158f3debb9c
Chen-Pang He <chen.pang.he@jdh8.org>
2024-04-25 01:36:52 +0800

    Untested conversion from float

--- minifloat.hpp
+++ minifloat.hpp
@@ -9,7 +9,6 @@
 #ifndef SKYMIZER_MINIFLOAT_HPP
 #define SKYMIZER_MINIFLOAT_HPP
 
-#include <cfloat>
 #include <limits>
 #include <type_traits>
 #include <cmath>
@@ -34,16 +33,23 @@ auto bit_cast(const From &from) noexcept -> std::enable_if_t<
   return caster.to;
 #endif
 }
-} // namespace detail
 
-/** \brief Internal storage type for minifloats
-  * \tparam S - Signedness
-  * \tparam E - Exponent width
+/** \brief Round a float to the nearest float with the given significand width
   * \tparam M - Significand (mantissa) width
   */
-template <bool S, unsigned E, unsigned M>
-using StorageType = std::enable_if_t<S + E + M <= 16,
-  std::conditional_t<S + E + M <= 8, std::uint_least8_t, std::uint_least16_t>>;
+template <unsigned M>
+[[nodiscard, gnu::const]]
+float round_to_mantissa(float x) {
+  static_assert(std::numeric_limits<float>::radix == 2);
+  static_assert(M < std::numeric_limits<float>::digits);
+  static_assert(std::numeric_limits<float>::is_iec559);
+
+  const auto bits = bit_cast<std::uint32_t>(x);
+  const auto ulp = std::uint32_t{1} << (std::numeric_limits<float>::digits - 1 - M);
+  const auto bias = ulp / 2 - !(bits & ulp);
+  return bit_cast<float>((bits + bias) & ~(ulp - 1));
+}
+} // namespace detail
 
 /** \brief A fast native type borrowed for minifloat arithmetics
   * \tparam M - Significand (mantissa) width
@@ -70,8 +76,6 @@ enum struct SubnormalStyle {
   Fast,     ///< I am speed
 };
 
-} // namespace minifloat
-
 /** \brief Configurable floating point type
   * \tparam S - Signedness
   * \tparam E - Exponent width
@@ -86,21 +90,94 @@ enum struct SubnormalStyle {
   */
 template <bool S, unsigned E, unsigned M,
   int B = (1 << (E - 1)) - 1,
-  minifloat::NaNStyle N = minifloat::NaNStyle::IEEE,
-  minifloat::SubnormalStyle D = minifloat::SubnormalStyle::Precise>
-class Minifloat
-{
-  minifloat::StorageType<S, E, M> _bits;
-
+  NaNStyle N = NaNStyle::IEEE,
+  SubnormalStyle D = SubnormalStyle::Precise>
+class Minifloat {
 public:
+  static_assert(N != NaNStyle::FNUZ || S);
+  static_assert(S + E + M <= 16);
+  typedef std::conditional_t<S + E + M <= 8, std::uint_least8_t, std::uint_least16_t> StorageType;
+
   static const unsigned RADIX = 2;
   static const unsigned MANTISSA_DIGITS = M + 1;
-  static const int MAX_EXP = (1 << E) - 1 - B;
+  static const int MAX_EXP = (1 << E) - B - (N == NaNStyle::IEEE);
   static const int MIN_EXP = 2 - B;
 
+private:
+  StorageType _bits;
+
+  [[nodiscard, gnu::const]]
+  static constexpr StorageType _inf_bits() {
+    const StorageType MAX = (UINT32_C(1) << (E + M)) - 1;
+
+    if constexpr (N == NaNStyle::IEEE)
+      return MAX << M & MAX;
+
+    return MAX - (N == NaNStyle::FN);
+  }
+
+  [[nodiscard, gnu::const]]
+  static constexpr StorageType _nan_bits() {
+    const StorageType N0 = UINT32_C(1) << (E + M);
+    const StorageType MAX = N0 - 1;
+
+    if constexpr (N == NaNStyle::FNUZ)
+      return N0;
+
+    if constexpr (N == NaNStyle::FN || M == 0)
+      return MAX;
+
+    return MAX << (M - 1) & MAX;
+  }
+
+  [[nodiscard, gnu::const]]
+  static StorageType _to_bits(float x) {
+    const auto bits = detail::bit_cast<std::uint32_t>(detail::round_to_mantissa<M>(x));
+    const auto sign = S ? bits >> 31 << (E + M) : 0;
+
+    if (x != x) 
+      return sign | _nan_bits();
+
+    if (!S && x <= 0)
+      return 0;
+
+    const std::int32_t diff = MIN_EXP - std::numeric_limits<float>::min_exponent;
+    const std::int32_t bias = diff << (std::numeric_limits<float>::digits - 1);
+    const std::int32_t magnitude = (bits & INT32_MAX) - bias;
+
+    if (magnitude < std::int32_t{1} << (std::numeric_limits<float>::digits - 1)) {
+      if constexpr (D == SubnormalStyle::Precise) {
+        const StorageType ticks = std::rint(std::ldexp(std::abs(x), MANTISSA_DIGITS - MIN_EXP));
+        return (N != NaNStyle::FNUZ || ticks) * sign | ticks;
+      }
+      if (magnitude <= std::int32_t{1} << (std::numeric_limits<float>::digits - 2))
+        return (N != NaNStyle::FNUZ) * sign;
+      return sign | 1 << M;
+    }
+    const int shift = std::numeric_limits<float>::digits - MANTISSA_DIGITS;
+    return sign | std::min<std::int32_t>(magnitude >> shift, _inf_bits());
+  }
+
+public:
   Minifloat() = default;
-  explicit Minifloat(float);
-  explicit Minifloat(double);
+  explicit Minifloat(float x) : _bits(_to_bits(x)) {};
+
+  [[nodiscard, gnu::const]]
+  StorageType bits() const noexcept { return _bits; }
+
+  [[nodiscard, gnu::const]]
+  bool is_nan() const noexcept {
+    const unsigned MAGNITUDE_BITS = E + M;
+    const StorageType MAX_MAGNITUDE = (1U << MAGNITUDE_BITS) - 1U;
+
+    if constexpr (N == NaNStyle::FNUZ)
+      return _bits == MAX_MAGNITUDE + 1U;
+
+    if constexpr (N == NaNStyle::FN)
+      return (_bits & MAX_MAGNITUDE) == MAX_MAGNITUDE;
+
+    return (_bits & MAX_MAGNITUDE) > ((1U << E) - 1U) << M;
+  }
 
   //TODO: Explicit inexact conversion
   [[nodiscard, gnu::const]]
@@ -114,22 +191,14 @@ public:
     const unsigned MAGNITUDE_BITS = E + M;
     const std::uint32_t MAX_MAGNITUDE = (1U << MAGNITUDE_BITS) - 1U;
     const std::uint32_t sign = S && _bits >> MAGNITUDE_BITS;
-    const std::uint32_t magnitude = _bits & MAX_MAGNITUDE;
 
-    if (N == minifloat::NaNStyle::FNUZ && _bits == MAX_MAGNITUDE + 1U)
-      return NAN;
+    if (is_nan())
+      return std::copysign(NAN, sign ? -1.0f : 1.0f);
 
-    if (N == minifloat::NaNStyle::FN && magnitude == MAX_MAGNITUDE)
-      return sign ? -NAN : NAN;
-
-    const std::uint32_t shifted = magnitude << (std::numeric_limits<float>::digits - MAGNITUDE_BITS);
-
-    if (N == minifloat::NaNStyle::IEEE && magnitude > ((1U << E) - 1U) << M)
-      return minifloat::detail::bit_cast<float>(sign << 31 | 0x7FC00000 | shifted);
-
+    const std::uint32_t magnitude = (_bits & MAX_MAGNITUDE) << (std::numeric_limits<float>::digits - MAGNITUDE_BITS);
     const std::uint32_t diff = MIN_EXP - std::numeric_limits<float>::min_exponent;
     const std::uint32_t bias = diff << (std::numeric_limits<float>::digits - 1);
-    return minifloat::detail::bit_cast<float>(sign << 31 | (shifted + bias));
+    return detail::bit_cast<float>(sign << 31 | (magnitude + bias));
   }
 
   //TODO: Explicit inexact conversion
@@ -144,51 +213,47 @@ public:
     const unsigned MAGNITUDE_BITS = E + M;
     const std::uint64_t MAX_MAGNITUDE = (1U << MAGNITUDE_BITS) - 1U;
     const std::uint64_t sign = S && _bits >> MAGNITUDE_BITS;
-    const std::uint64_t magnitude = _bits & MAX_MAGNITUDE;
 
-    if (N == minifloat::NaNStyle::FNUZ && _bits == MAX_MAGNITUDE + 1U)
-      return NAN;
+    if (is_nan())
+      return std::copysign(NAN, sign ? -1.0 : 1.0);
 
-    if (N == minifloat::NaNStyle::FN && magnitude == MAX_MAGNITUDE)
-      return sign ? -NAN : NAN;
-
-    const std::uint64_t shifted = magnitude << (std::numeric_limits<double>::digits - MAGNITUDE_BITS);
-
-    if (N == minifloat::NaNStyle::IEEE && magnitude > ((1U << E) - 1U) << M)
-      return minifloat::detail::bit_cast<double>(sign << 63 | 0x7FF8000000000000 | shifted);
-
+    const std::uint64_t magnitude = (_bits & MAX_MAGNITUDE) << (std::numeric_limits<double>::digits - MAGNITUDE_BITS);
     const std::uint64_t diff = MIN_EXP - std::numeric_limits<double>::min_exponent;
     const std::uint64_t bias = diff << (std::numeric_limits<double>::digits - 1);
-    return minifloat::detail::bit_cast<double>(sign << 63 | (shifted + bias));
+    return detail::bit_cast<double>(sign << 63 | (magnitude + bias));
   }
 };
 
-template <bool S, unsigned E, unsigned M, int B, minifloat::NaNStyle N, minifloat::SubnormalStyle D>
+template <bool S, unsigned E, unsigned M, int B, NaNStyle N, SubnormalStyle D>
 [[gnu::const]]
 Minifloat<S, E, M, B, N, D> operator+(const Minifloat<S, E, M, B, N, D> &x, const Minifloat<S, E, M, B, N, D> &y) noexcept {
-  typedef minifloat::BitTrueGroupArithmeticType<M> ArithmeticType;
+  typedef BitTrueGroupArithmeticType<M> ArithmeticType;
   return static_cast<ArithmeticType>(x) + static_cast<ArithmeticType>(y);
 }
 
-template <bool S, unsigned E, unsigned M, int B, minifloat::NaNStyle N, minifloat::SubnormalStyle D>
+template <bool S, unsigned E, unsigned M, int B, NaNStyle N, SubnormalStyle D>
 [[gnu::const]]
 Minifloat<S, E, M, B, N, D> operator-(const Minifloat<S, E, M, B, N, D> &x, const Minifloat<S, E, M, B, N, D> &y) noexcept {
-  typedef minifloat::BitTrueGroupArithmeticType<M> ArithmeticType;
+  typedef BitTrueGroupArithmeticType<M> ArithmeticType;
   return static_cast<ArithmeticType>(x) - static_cast<ArithmeticType>(y);
 }
 
-template <bool S, unsigned E, unsigned M, int B, minifloat::NaNStyle N, minifloat::SubnormalStyle D>
+template <bool S, unsigned E, unsigned M, int B, NaNStyle N, SubnormalStyle D>
 [[gnu::const]]
 Minifloat<S, E, M, B, N, D> operator*(const Minifloat<S, E, M, B, N, D> &x, const Minifloat<S, E, M, B, N, D> &y) noexcept {
-  typedef minifloat::BitTrueGroupArithmeticType<M> ArithmeticType;
+  typedef BitTrueGroupArithmeticType<M> ArithmeticType;
   return static_cast<ArithmeticType>(x) * static_cast<ArithmeticType>(y);
 }
 
-template <bool S, unsigned E, unsigned M, int B, minifloat::NaNStyle N, minifloat::SubnormalStyle D>
+template <bool S, unsigned E, unsigned M, int B, NaNStyle N, SubnormalStyle D>
 [[gnu::const]]
 Minifloat<S, E, M, B, N, D> operator/(const Minifloat<S, E, M, B, N, D> &x, const Minifloat<S, E, M, B, N, D> &y) noexcept {
   return static_cast<double>(x) / static_cast<double>(y);
 }
+
+} // namespace minifloat
+
+using minifloat::Minifloat;
 
 } // namespace skymizer
 
