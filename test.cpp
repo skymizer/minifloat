7abe43d7cca164b2348f349b3b187231a653b07c
Chen-Pang He <chen.pang.he@jdh8.org>
2024-04-30 01:02:18 +0800

    Introduce typedefs like Eigen

--- test.cpp
+++ test.cpp
@@ -1,34 +1,35 @@
 #include "minifloat.hpp"
 #include <gtest/gtest.h>
 
-using skymizer::Minifloat;
-using N = skymizer::minifloat::NaNStyle;
+using namespace skymizer::minifloat;
 
 template <typename T>
 T id(T x) { return x; }
 
 TEST(SanityCheck, truncate) {
-  EXPECT_EQ((Minifloat<3, 4>{2.0f}.bits()), 0x40);
-  EXPECT_EQ((Minifloat<4, 3>{2.0f}.bits()), 0x40);
-  EXPECT_EQ((Minifloat<5, 2>{2.0f}.bits()), 0x40);
+  EXPECT_EQ(E3M4{2.0f}.bits(), 0x40);
+  EXPECT_EQ(E4M3{2.0f}.bits(), 0x40);
+  EXPECT_EQ(E5M2{2.0f}.bits(), 0x40);
 
-  EXPECT_EQ((Minifloat<3, 4>{1.0f}.bits()), 0b0'011'0000);
-  EXPECT_EQ((Minifloat<4, 3>{1.0f}.bits()), 0b0'0111'000);
-  EXPECT_EQ((Minifloat<5, 2>{1.0f}.bits()), 0b0'01111'00);
+  EXPECT_EQ(E3M4{1.0f}.bits(), 0b0'011'0000);
+  EXPECT_EQ(E4M3{1.0f}.bits(), 0b0'0111'000);
+  EXPECT_EQ(E5M2{1.0f}.bits(), 0b0'01111'00);
 
-  EXPECT_EQ((Minifloat<3, 4>{-1.25f}.bits()), 0b1'011'0100);
-  EXPECT_EQ((Minifloat<4, 3>{-1.25f}.bits()), 0b1'0111'010);
-  EXPECT_EQ((Minifloat<5, 2>{-1.25f}.bits()), 0b1'01111'01);
+  EXPECT_EQ(E3M4{-1.25f}.bits(), 0b1'011'0100);
+  EXPECT_EQ(E4M3{-1.25f}.bits(), 0b1'0111'010);
+  EXPECT_EQ(E5M2{-1.25f}.bits(), 0b1'01111'01);
 }
 
 TEST(SanityCheck, equality) {
-  EXPECT_EQ(id<float>(Minifloat<3, 4>{-3.0f}), -3.0f);
-  EXPECT_EQ(id<float>(Minifloat<4, 3>{-3.0f}), -3.0f);
-  EXPECT_EQ(id<float>(Minifloat<5, 2>{-3.0f}), -3.0f);
+  EXPECT_EQ(id<float>(E3M4{-3.0f}), -3.0f);
+  EXPECT_EQ(id<float>(E4M3{-3.0f}), -3.0f);
+  EXPECT_EQ(id<float>(E5M2{-3.0f}), -3.0f);
 
-  EXPECT_EQ(id<double>(Minifloat<3, 4>{-3.0}), -3.0);
-  EXPECT_EQ(id<double>(Minifloat<4, 3>{-3.0}), -3.0);
-  EXPECT_EQ(id<double>(Minifloat<5, 2>{-3.0}), -3.0);
+  EXPECT_EQ(id<double>(E3M4{-3.0}), -3.0);
+  EXPECT_EQ(id<double>(E4M3{-3.0}), -3.0);
+  EXPECT_EQ(id<double>(E5M2{-3.0}), -3.0);
 
-  EXPECT_TRUE((Minifloat<4, 3, N::IEEE>{NAN}.is_nan()));
+  EXPECT_TRUE(E4M3{NAN}.is_nan());
+  EXPECT_TRUE(E4M3FN{NAN}.is_nan());
+  EXPECT_TRUE(E4M3FNUZ{NAN}.is_nan());
 }
