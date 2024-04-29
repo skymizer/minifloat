6fb7a51cd859f60201e8ac6a42eb84e70092454b
Chen-Pang He <chen.pang.he@jdh8.org>
2024-04-30 04:18:59 +0800

    Harden test for finite bit representations

--- test.cpp
+++ test.cpp
@@ -19,21 +19,28 @@ struct Trait<Minifloat<E_, M_, N_, B_, D_>> {
 template <typename T>
 static T id(T x) { return x; }
 
-TEST(SanityCheck, truncate) {
-  EXPECT_EQ(E3M4{2.0f}.bits(), 0x40);
-  EXPECT_EQ(E4M3{2.0f}.bits(), 0x40);
-  EXPECT_EQ(E5M2{2.0f}.bits(), 0x40);
-  EXPECT_EQ(E5M7{2.0f}.bits(), 0b0'10000'0000000);
+template <unsigned E, unsigned M>
+static void test_finite_bits(float x, unsigned bits) {
+  EXPECT_EQ((Minifloat<E, M>{x}.bits()), bits);
+  EXPECT_EQ((Minifloat<E, M, NaNStyle::FN>{x}.bits()), bits);
+  EXPECT_EQ((Minifloat<E, M, NaNStyle::FNUZ>{x}.bits()), bits);
+}
 
-  EXPECT_EQ(E3M4{1.0f}.bits(), 0b0'011'0000);
-  EXPECT_EQ(E4M3{1.0f}.bits(), 0b0'0111'000);
-  EXPECT_EQ(E5M2{1.0f}.bits(), 0b0'01111'00);
-  EXPECT_EQ(E5M7{1.0f}.bits(), 0b0'01111'0000000);
+TEST(SanityCheck, finite_bits) {
+  test_finite_bits<3, 4>(2.0f, 0x40);
+  test_finite_bits<4, 3>(2.0f, 0x40);
+  test_finite_bits<5, 2>(2.0f, 0x40);
+  test_finite_bits<5, 7>(2.0f, 0b0'10000'0000000);
 
-  EXPECT_EQ(E3M4{-1.25f}.bits(), 0b1'011'0100);
-  EXPECT_EQ(E4M3{-1.25f}.bits(), 0b1'0111'010);
-  EXPECT_EQ(E5M2{-1.25f}.bits(), 0b1'01111'01);
-  EXPECT_EQ(E5M7{-1.25f}.bits(), 0b1'01111'0100000);
+  test_finite_bits<3, 4>(1.0f, 0b0'011'0000);
+  test_finite_bits<4, 3>(1.0f, 0b0'0111'000);
+  test_finite_bits<5, 2>(1.0f, 0b0'01111'00);
+  test_finite_bits<5, 7>(1.0f, 0b0'01111'0000000);
+
+  test_finite_bits<3, 4>(-1.25f, 0b1'011'0100);
+  test_finite_bits<4, 3>(-1.25f, 0b1'0111'010);
+  test_finite_bits<5, 2>(-1.25f, 0b1'01111'01);
+  test_finite_bits<5, 7>(-1.25f, 0b1'01111'0100000);
 }
 
 template <typename T>
