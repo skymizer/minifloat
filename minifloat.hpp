0c75a5ae52136c469545c0986424ceefdb6714a4
Chen-Pang He <chen.pang.he@jdh8.org>
2024-05-06 21:08:19 +0800

    Relicense under MPL for explicit patent licensing

--- minifloat.hpp
+++ minifloat.hpp
@@ -1,11 +1,11 @@
-//===- minifloat.hpp ------------------------------------------------------===//
-//
 // This file is part of the minifloat project of Skymizer.
 //
-// This file is distributed under the New BSD License.
-// See LICENSE for details.
+// Copyright (C) 2024 Chen-Pang He <jdh8@skymizer.com>
 //
-//===----------------------------------------------------------------------===//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #ifndef SKYMIZER_MINIFLOAT_HPP
 #define SKYMIZER_MINIFLOAT_HPP
 
--- test.cpp
+++ test.cpp
@@ -1,3 +1,11 @@
+// This file is part of the minifloat project of Skymizer.
+//
+// Copyright (C) 2024 Chen-Pang He <jdh8@skymizer.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #include "minifloat.hpp"
 #include <gtest/gtest.h>
 
